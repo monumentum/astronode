99393876b1c1f1e28ab1149de9e88acf9cd4c46b
Guilherme Diego <guidiego.expgames@gmail.com>
2017-10-16 22:10:56 -0200

    Fix: restoose: coveralls and bithound problems

--- example/express/modules/beer/model.js
+++ example/express/modules/beer/model.js
@@ -2,6 +2,6 @@ const RestooseModel = require('../../../../restoose/model');
 
 const structure = {
     name: RestooseModel.string().isRequired()
-}
+};
 
 module.exports = RestooseModel.register('Beer', structure);
