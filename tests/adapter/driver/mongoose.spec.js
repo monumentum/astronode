c7b4780e179df450a61168094f47ee68eca5221e
Guilherme Diego <guidiego.expgames@gmail.com>
2018-01-16 18:46:47 -0200

    Doing: Add tests to mongoose driver

--- tests/adapter/driver/mongoose.spec.js
+++ tests/adapter/driver/mongoose.spec.js
@@ -1,13 +1,40 @@
-// const uri = 'test.com';
-        // const port = 1000;
-        // const database = 'test'
-        // const user = 'test_user';
+const MongooseAdapter = require('../../../src/adapter/driver/mongoose');
 
-        // it('should exec #getDriver correctly', () => {
-        //     const driver = adapter.getDriver({driver: 'mongoose', driverConfig: {uri, port, database, user}});
-        //     expect(driver).toHaveProperty('uri', `mongodb://${uri}:${port}/${database}`);
-        //     expect(driver).
-        // });
+const expectOn = (call, index, method) =>
+    expect(call.mock.calls[index][0]).toBe(method);
+
 describe('src/adapter/driver/mongoose', () => {
-    it('should start', () => {});
-})
+    let adapter;
+
+    const uri = 'test.com';
+    const port = 1000;
+    const database = 'test'
+    const user = 'test_user';
+
+    const fakeConnOn = jest.fn((name, cb) => cb());
+
+    beforeEach(() => {
+        adapter = new MongooseAdapter({ uri, port, database, user });
+    });
+
+    it('should start correctly', () => {
+        expect(adapter).toHaveProperty('uri', `mongodb://${uri}:${port}/${database}`);
+        expect(adapter).toHaveProperty('opts', { user });
+    });
+
+    it('exec start correctly', () => {
+        adapter._registerModels = jest.fn();
+        adapter.mongoose = {
+            createConnection: jest.fn().mockReturnValue({
+                on: fakeConnOn,
+            }),
+        };
+
+        return adapter.start().then(() => {
+            expectOn(fakeConnOn, 0, 'error');
+            expectOn(fakeConnOn, 1, 'disconnected');
+            expectOn(fakeConnOn, 2, 'connected');
+            expect(adapter.mongoose.createConnection).toHaveBeenCalledWith(adapter.uri, adapter.opts);
+        });
+    });
+});
